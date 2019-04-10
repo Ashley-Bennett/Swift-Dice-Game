0c69de0ea2874341c0526844c39acb37dfd2ee66
Ashley Bennett <codenation@CN-20.local>
2019-04-10 16:25:39 +0100

    Initial Commit

---
+++ dice/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  dice
+//
+//  Created by Code Nation 20 on 10/04/2019.
+//  Copyright © 2019 Code Nation 20. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ dice/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  dice
+//
+//  Created by Code Nation 20 on 10/04/2019.
+//  Copyright © 2019 Code Nation 20. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
